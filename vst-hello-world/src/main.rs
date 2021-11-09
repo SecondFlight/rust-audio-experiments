e3af20fe0084f165885b840fb43f42cfc9eb3c3f
Joshua Wade <SecondFlight@users.noreply.github.com>
2021-11-09 00:13:38 -0500

    Add plugin window that doesn't actually open

--- vst-hello-world/src/main.rs
+++ vst-hello-world/src/main.rs
@@ -1,10 +1,13 @@
 extern crate vst;
 
-use std::sync::{Arc, Mutex};
 use std::path::Path;
+use std::sync::{Arc, Mutex};
 
 use vst::host::{Host, PluginLoader};
 use vst::plugin::Plugin;
+use winapi::shared::minwindef::HINSTANCE__;
+use winapi::shared::windef::{HMENU__};
+use winapi::um::winuser::HWND_DESKTOP;
 
 struct SampleHost;
 
@@ -31,9 +34,38 @@ fn main() {
     }
 
     instance.init();
-    // println!("Initialized instance!");
+    println!("Initialized instance!");
 
-    // println!("Closing instance...");
+    let mut editor = instance.get_editor().unwrap();
+    let size = editor.size();
+
+    // this is very gross
+    // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexa
+    let parent;
+    unsafe {
+        parent = winapi::um::winuser::CreateWindowExW(
+            0,
+            "STATIC".as_bytes().as_ptr() as *const u16,
+            "editor".as_bytes().as_ptr() as *const u16,
+            0x10000000,
+            0,
+            0,
+            size.0,
+            size.1,
+            HWND_DESKTOP,
+            0 as *mut HMENU__,
+            0 as *mut HINSTANCE__,
+            0 as *mut winapi::ctypes::c_void,
+        );
+        winapi::um::winuser::ShowWindow(parent, 1);
+    }
+    let successful = editor.open(parent as *mut std::ffi::c_void);
+    
+    println!("could open editor with provided window: {}", successful);
+
+    std::thread::sleep(std::time::Duration::from_millis(10000));
+
+    println!("Closing instance...");
     // Not necessary as the instance is shut down when it goes out of scope anyway.
     // drop(instance);
 }
