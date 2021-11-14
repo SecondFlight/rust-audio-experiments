e75a564fbc6b57e9406a00c2825f7502a3de4760
Joshua Wade <SecondFlight@users.noreply.github.com>
2021-11-13 23:08:50 -0500

    Remove nodegraph

--- anthem-nodegraph/src/node.rs
+++
@@ -1,8 +0,0 @@
-use std::collections::HashMap;
-
-pub trait Port {}
-
-pub trait Node<'a> {
-    fn input_ports() -> &'a HashMap<u64, Box<dyn Port>>;
-    fn output_ports() -> &'a HashMap<u64, Box<dyn Port>>;
-}
